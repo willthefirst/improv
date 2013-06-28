ec53f00be7fb90ecce4550b15533df937255ed67
Will Moritz <will@crowdfavorite.com>
2013-06-27 20:49:19 -0600

    updated crowd suggestion:

--- wp-content/themes/improv/assets/js/script.js
+++ wp-content/themes/improv/assets/js/script.js
@@ -4,25 +4,30 @@ jQuery(function($) {
 	// Add support for placeholder attribute in older browsers.
 	$('.search-input').placeholder();
 
-	$('.crowd-suggestion').submit(function(e) {
-		e.preventDefault();
 
-		$('.crowd-suggestion-result').hide();
-		$('.processing').show();
-		$('.crowd-suggestion-container').addClass("spin");
+	$(function() {
+		$('.crowd-suggestion-container').show();
+		$('.crowd-suggestion').submit(function(e) {
+			e.preventDefault();
 
-		var query = ($('.crowd-suggestion-value')[0].value);
-		var image_url, style;
+			$('.crowd-suggestion-result').hide();
+			$('.processing').show();
+			$('.crowd-suggestion-container').addClass("spin");
 
-		$.getJSON('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' + query + '&callback=?', function(data) {
-		  image_url = data.responseData.results[0].tbUrl;
-		  style = 'url("' + image_url + '");';
-		  $('body').css('background-image', 'url(' + image_url + ')');
-		  $('.processing').hide();
-		  $('.crowd-suggestion-container').removeClass("spin");
-		  $('.crowd-suggestion-result').show();
+			var query = ($('.crowd-suggestion-value')[0].value);
+			var image_url, style;
 
+			$.getJSON('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' + query + '&callback=?', function(data) {
+			  image_url = data.responseData.results[0].tbUrl;
+			  style = 'url("' + image_url + '");';
+			  $('body').css('background-image', 'url(' + image_url + ')');
+			  $('.processing').hide();
+			  $('.crowd-suggestion-container').removeClass("spin");
+			  $('.crowd-suggestion-result').show();
+
+			});
 		});
 	});
+
 
 });
